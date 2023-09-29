74c550027cccbbce26f6c5fa9b0fbda6e7ff5de7
srikanth <sri@123>
2023-09-29 12:04:15 +0530

    list task

---
+++ listtask.py
@@ -0,0 +1,86 @@
+#write a python program to merge two lists.
+#list_1 = [1,2,3]
+#list_2 = [4,5,6]
+#list_3 = list_1 + list_2 
+#print(list_3)
+
+#write a python program to find the sum of list elements.
+#total = 0 
+#list_1 = [1,2,3]
+#for i in range(0, len(list_1)):
+ #   total = total + list_1[i] 
+#print(total)
+#write a python program to print even and odd numbers seperatly in list.
+
+#num_list = [1,2,3,4,5,6,7,8,9]
+#even_list = []
+#odd_list = []
+#for i in range(len(num_list)):
+ #   if (num_list[i] % 2 == 0):
+  #      even_list.append(num_list[i])
+   # else:
+    #    odd_list.append(num_list[i])
+#print(even_list)
+#print(odd_list)
+
+#write a python program to delete element of given index in list.
+#inputList = ["i","am", "srikanth",]
+#givenIndex = 2 
+#del inputList[givenIndex]
+#print(inputList)
+
+#.write a python program to delete given elemet from the list 
+#my_list = [1,2,3,4,5,4]
+#print(my_list)
+#print(my_list.pop(2))
+#print(my_list)
+
+#write a python program to insert an elemet  at given index location.
+#given_1 = [1,2,3,4,5,6,7]
+#given_1.insert(2,45)
+#print(given_1)
+
+#write a python program to check the sizes of given two lists are same.
+#list_1 = [1,2,3,4]
+#list_2 = [5,6,7,8]
+#for i in range(len(list_1)):
+ #   if list_1[i] == list_2[i]:
+  #      print("same")
+    #else:
+    #    print("different")
+    
+#.Write a Python function that takes two lists and returns True if they have at least one common member.
+#list_1 = [11,22,33,44,55]
+#list_2 = [55,66,77,88,99]
+#total = False
+#for i in list_1:
+ #  for j in list_2:
+  #      if i == j:
+   #         total = True
+    #        print(total)
+#if total:
+ #   print("list have at least one common number")
+#else:
+ #   ("list do not have any common element")
+        
+
+
+#Write a Python program to remove a specified column from a given nested list.
+#given_list = [[1, 2, 3], [2, 4, 5], [1, 1, 1]]
+#for row in given_list:
+ #   del row[0]
+#print(given_list)
+
+#9. Write a Python program to convert a list of multiple integers into a single integer.
+#list_1 = [11, 33, 50]
+#print(list_1)
+#num = int("".join(map(str, list_1)))
+#print(num)
+
+#10.Write a Python program to remove duplicates from a list.
+#list_1 = [1,2,3,4,2,5,6,7,4]
+#empty_list = []
+#for i in list_1:
+ #   if i not in empty_list:
+  #      empty_list.append(i)
+#print(empty_list)
